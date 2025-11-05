ab8bb20dfd67d43dae32d1d0ce82c6a6a79fed0f
Frans Mazhar <frans.mazhar@gmail.com>
2025-11-05 15:35:18 +0700

    upd: is_user_ords_exist

--- ords/scripts/is_user_ords_exist.sh
+++ ords/scripts/is_user_ords_exist.sh
@@ -1,14 +1,13 @@
-sql -s sys/${ORACLE_PWD}@db:1521/XEPDB1 as sysdba <<EOF
+sql -s sys/${ORACLE_PWD}@db:1521/XEPDB1 as sysdba &> /dev/null <<EOF
 
 WHENEVER SQLERROR EXIT FAILURE;
 SET FEEDBACK OFF;
 DECLARE
     l_username VARCHAR2(30);
 BEGIN
-    SELECT username INTO l_username FROM dba_users WHERE username = 'ORDS_PUBLIC_USER';
+    SELECT username INTO l_username FROM dba_users WHERE username = 'ORDS_PUBLIC_USR';
 END;
 /
-echo "User ORDS are exist."
 exit;
 EOF
 
